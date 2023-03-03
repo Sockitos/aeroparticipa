bc25541a220b65c54074f2c72e9f0e5e49ccf6f0
João Nogueira <jrnogueira@edu.ulisboa.pt>
2023-03-03 13:43:09 +0000

    Add page

--- src/routes/(sobre-a-cti)/regimento-da-cti/+page.svelte
+++ src/routes/(sobre-a-cti)/regimento-da-cti/+page.svelte
@@ -1,3 +1,251 @@
+<script>
+	import PageContainer from '@/components/PageContainer.svelte';
+
+	import PageHeader from '@/components/PageHeader.svelte';
+</script>
+
 <svelte:head>
 	<title>AeroParticipa - Regimento da Comissão Técnica Independente</title>
 </svelte:head>
+
+<PageHeader>Regimento da<br />Comissão Técnica Independente</PageHeader>
+<PageContainer>
+	<p>
+		Análise estratégica e multidisciplinar do aumento da capacidade aeroportuária da região de
+		Lisboa - Resolução do Conselho de Ministros n.o 89/2022
+	</p>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 1º<br />Objeto</h3>
+	<p>
+		O presente regulamento estabelece as normas de organização e funcionamento da Comissão Técnica
+		Independente (CTI), constituída pela Resolução do Conselho de Ministros n.o 89/2022, de 14 de
+		outubro, para avaliar as opções estratégicas para aumentar a capacidade aeroportuária da região
+		de Lisboa e coordenar e realizar a avaliação ambiental estratégica.
+	</p>
+	<h3 class="text-2xl font-bold text-center pt-6">
+		Artigo 2º<br />Natureza, missões e competências
+	</h3>
+	<ol class="marker:text-blue-accent marker:font-bold list-decimal list-outside pl-10">
+		<li>A CTI é um órgão colegial, independente, sem personalidade jurídica.</li>
+		<li>
+			A CTI atua de forma independente, não podendo os seus membros solicitar nem receber ordens,
+			instruções ou orientações do Governo ou de qualquer entidade pública ou privada, sem prejuízo
+			das competências da Comissão de Acompanhamento previstas na Resolução de Conselho de Ministros
+			n.o 89/2022, de 14 de outubro.
+		</li>
+		<li>
+			A CTI tem por missão avaliar as opções estratégicas para aumentar a capacidade aeroportuária
+			da região de Lisboa e coordenar e realizar a avaliação ambiental estratégica (AAE).
+		</li>
+		<li>
+			A CTI orienta-se pelo estabelecido na Resolução de Conselho de Ministros n.o 89/2022, de 14 de
+			outubro, com as alterações já submetidas ao conhecimento do Sr. Ministro das Infra-Estruturas
+			e do Gabinete do Senhor Ministro em 6 de Janeiro de 2023, e que aguardam re-publicação.
+		</li>
+		<li>
+			Para o cumprimento da missão prevista no número anterior, compete à CTI:
+			<ol class="marker:text-blue-accent marker:font-bold list-[lower-alpha] list-outside pl-10">
+				<li>
+					Realizar, rever e avaliar os estudos, projetos e planos sobre as opções estratégicas
+					identificadas para aumentar a capacidade aeroportuária da região de Lisboa.
+				</li>
+				<li>
+					Realizar, rever e avaliar os estudos, projetos e planos sobre outras opções estratégicas
+					para aumentar a capacidade aeroportuária da região de Lisboa, desde que tecnicamente
+					fundamentadas.
+				</li>
+				<li>
+					Realizar a AAE, prevista no Decreto-Lei n.o 232/2007, de 15 de julho, na sua redação
+					atual, dos estudos, projetos e planos mencionados nas alíneas anteriores, designadamente
+					nas dimensões aeroportuária, operacional, acessibilidades, incluindo das infraestruturas
+					conexas e complementares, económica, financeira, social, jurídica e ambiental.
+				</li>
+			</ol>
+		</li>
+	</ol>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 3º<br />Composição</h3>
+	<ol class="marker:text-blue-accent marker:font-bold list-decimal list-outside pl-10">
+		<li>
+			A CTI é composta por:
+			<ol class="marker:text-blue-accent marker:font-bold list-[lower-alpha] list-outside pl-10">
+				<li>
+					Uma Coordenadora-Geral, designada pelo Primeiro-Ministro, sob proposta conjunta do
+					presidente do Conselho Superior de Obras Públicas (CSOP), do Conselho Nacional do Ambiente
+					e do Desenvolvimento Sustentável (CNADS) e do Conselho de Reitores das Universidades
+					Portuguesas (CRUP), no prazo de 30 dias contados da data de publicação da presente
+					resolução.
+				</li>
+				<li>
+					Seis coordenadores de projeto que preenchem as qualificações e competências técnicas
+					adequadas nas seguintes áreas de trabalho das seis equipas de projeto:
+					<ol
+						class="marker:text-blue-accent marker:font-bold list-[lower-roman] list-outside pl-10"
+					>
+						<li>
+							Estudos de procura aeroportuários e de acessibilidades de infraestruturas e
+							transportes;
+						</li>
+						<li>
+							Planificação aeroportuária, incluindo análise de capacidade e planos de
+							desenvolvimento aeroportuário compatíveis com a evolução de um hub intercontinental;
+						</li>
+						<li>Acessibilidades rodoviárias e ferroviárias;</li>
+						<li>Ambiente;</li>
+						<li>Análise e modelação económico-financeira;</li>
+						<li>Jurídica.</li>
+					</ol>
+				</li>
+				<li>
+					Os coordenadores das equipas de projeto referidos no número anterior são especialistas das
+					respetivas áreas de trabalho da Comissão Técnica, designados pela coordenadora-geral, sob
+					proposta do CRUP.
+				</li>
+			</ol>
+		</li>
+	</ol>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 4º<br />Coordenadora Geral da CTI</h3>
+	<p>Compete à Coordenadora-Geral da CTI:</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-[lower-alpha] list-outside pl-10">
+		<li>Representar a CTI;</li>
+		<li>Designar os coordenadores de equipas de projeto e propor o seu nível de remuneração;</li>
+		<li>Convocar as reuniões e fixar a respetiva ordem dos trabalhos;</li>
+		<li>
+			Coordenar o conjunto das áreas temáticas e o contributo de cada um dos seus coordenadores;
+		</li>
+		<li>
+			Assegurar a condução dos trabalhos de acordo com o Plano de Trabalho e o cronograma
+			estabelecido;
+		</li>
+		<li>
+			Assegurar a prestação de esclarecimentos que sejam solicitados pela Comissão de
+			Acompanhamento;
+		</li>
+		<li>
+			Apoiar os coordenadores das áreas temáticas na identificação das respetivas equipas técnicas,
+			e no estabelecimento dos termos de referência;
+		</li>
+		<li>
+			Decidir a contratação de equipas externas, sob proposta do coordenador da respetiva área
+			temática, e propor ao LNEC a sua contratação;
+		</li>
+		<li>
+			Garantir que os resultados da avaliação se baseiam na integração de todas as áreas temáticas;
+		</li>
+		<li>
+			Assegurar o cumprimento dos prazos de acordo com o cronograma em tudo o que dependa da sua
+			decisão e capacidade de ação;
+		</li>
+		<li>Assegurar a preparação dos relatórios devidos, de acordo com o plano de trabalhos;</li>
+		<li>Assegurar um processo participado usando ferramentas digitais.</li>
+	</ol>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 5º<br />Coordenadores de equipa</h3>
+	<p>Compete aos coordernadores de equipa:</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-[lower-alpha] list-outside pl-10">
+		<li>
+			Identificar e coordenar as equipas técnicas na condução dos estudos técnicos necessários;
+		</li>
+		<li>
+			Assegurar que os estudos técnicos em cada uma das áreas temáticas entregam factos e evidências
+			que suportam os critérios de viabilidade técnico-científica, os fatores críticos de decisão
+			bem como a avaliação final de opções estratégicas no âmbito da avaliação ambiental
+			estratégica;
+		</li>
+		<li>
+			Contribuir para garantir que a avaliação é integrada e estratégica, considerando cada uma das
+			áreas temáticas e entre todas as áreas temáticas, de acordo com o plano de trabalhos;
+		</li>
+		<li>
+			Assegurar a preparação dos relatórios da respetiva área temática, de acordo com o plano de
+			trabalhos e em articulação com a Coordenadora-Geral;
+		</li>
+		<li>Cumprir os prazos estabelecidos no cronograma e as orientações da coordenadora- geral;</li>
+		<li>
+			Prestar os esclarecimentos necessários que sejam solicitados pela Comissão de Acompanhamento e
+			participar na reunião com a Comissão de Acompanhamento se assim forem solicitados pela
+			Coordenadora-Geral;
+		</li>
+		<li>Contribuir para a condução de um processo participado;</li>
+		<li>Propor à Coordenadora-Geral a contratação de equipas externas.</li>
+	</ol>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 6º<br />Reuniões</h3>
+	<ol class="marker:text-blue-accent marker:font-bold list-decimal list-outside pl-10">
+		<li>
+			A CTI funciona com carácter permanente, com a realização de reuniões ordinárias e
+			extraordinárias.
+		</li>
+		<li>
+			As reuniões da CTI são convocadas pela Coordenadora-Geral, por sua iniciativa ou a pedido de,
+			pelos menos, dois coordenadores de equipa.
+		</li>
+		<li>
+			As convocatórias são feitas pelo meio que a Coordenadora-Geral considerar mais adequado,
+			devendo constar o dia, hora e local da reunião, bem como a ordem de trabalhos.
+		</li>
+		<li>
+			As reuniões ordinárias são realizadas com a periodicidade definida pela Coordenadora- Geral
+			nos termos adequados ao desempenho das funções da CTI, devendo ser comunicadas aos restantes
+			membros qualquer alteração de dia ou hora, com a antecedência de 48 horas relativamente à hora
+			prevista de início dos trabalhos.
+		</li>
+		<li>
+			As reuniões são presididas pela Coordenadora-Geral, ou nas sua ausência e impedimentos pelo
+			coordenador de equipa que a Coordenadora-Geral designar.
+		</li>
+		<li>
+			As reuniões da CTI não são públicas e realizam-se presencialmente na sala do LNEC que lhe está
+			destinada ou, por deliberação da sua Coordenadora-Geral, em qualquer outro local, ou ainda por
+			meios telemáticos.
+		</li>
+		<li>
+			Sem prejuízo do disposto nos números anteriores, sempre que a urgência e a natureza do assunto
+			o justifique, pode uma proposta de deliberação ou de parecer ser apreciada pelos membros da
+			CTI pelo meio que se mostre mais adequado, caso em que o ato jurídico é assinado pelo membro
+			respetivo e ratificado na reunião seguinte.
+		</li>
+		<li>Das reuniões da CTI é sempre lavrada ata.</li>
+	</ol>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 7º<br />Quórum e maioria deliberativa</h3>
+	<ol class="marker:text-blue-accent marker:font-bold list-decimal list-outside pl-10">
+		<li>
+			A CTI só pode deliberar quando esteja presente a maioria do número legal dos seus membros com
+			direito a voto.
+		</li>
+		<li>
+			Quando se não verifique na primeira convocação o quórum previsto no número anterior, deve ser
+			convocada nova reunião com um intervalo mínimo de 24 horas.
+		</li>
+		<li>
+			Quando reunida em segunda convocatória, a CTI pode deliberar desde que esteja presente um
+			terço dos seus membros com direito a voto.
+		</li>
+		<li>As deliberações são tomadas por maioria absoluta dos membros presentes à reunião.</li>
+		<li>Em caso de empate, a Coordenadora-Geral tem voto de qualidade.</li>
+	</ol>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 8º<br />Atas</h3>
+	<ol class="marker:text-blue-accent marker:font-bold list-decimal list-outside pl-10">
+		<li>
+			Em cada reunião é lavrada ata, a qual contém um resumo sucinto, nomeadamente dos assuntos
+			apreciados, opiniões e das deliberações tomadas.
+		</li>
+		<li>
+			As atas são assinadas pela Coordenadora-Geral e Secretária, salvo ausência de um ou de ambos,
+			com substituição por outros coordenadores.
+		</li>
+	</ol>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 9º<br />Deveres e impedimentos</h3>
+	<ol class="marker:text-blue-accent marker:font-bold list-decimal list-outside pl-10">
+		<li>
+			Os membros da CTI estão sujeitos ao dever de sigilo sobre todos os assuntos que lhe sejam
+			confiados e de que tenham conhecimento por causa do exercício das suas funções.
+		</li>
+		<li>
+			Os membros da CTI estão sujeitos às garantias de imparcialidade previstas no Código do
+			Procedimento Administrativo.
+		</li>
+	</ol>
+	<h3 class="text-2xl font-bold text-center pt-6">Artigo 10º<br />Entrada em vigor</h3>
+	<p>
+		O presente regulamento entra em vigor após ser aprovado pela CTI e publicado no respetivo <i
+			>site</i
+		>.
+	</p>
+</PageContainer>
