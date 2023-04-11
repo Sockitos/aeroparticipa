a4f20d7b650837eedddefed61d5a8cd5f94a6e4a
João Nogueira <jrnogueira@edu.ulisboa.pt>
2023-04-11 03:38:49 +0100

    Update content

--- src/routes/(estrategia-de-participacao)/mesas-tematicas/3/+page.svelte
+++ src/routes/(estrategia-de-participacao)/mesas-tematicas/3/+page.svelte
@@ -131,4 +131,195 @@
 		</li>
 		<li>Não esquecer as normas internacionais de que o Estado Português é signatário.</li>
 	</ol>
+	<h3 class="text-2xl font-bold pt-6">
+		Resultados 2ª Sessão<br />Ponderação de custos escondidos, valorização dos ecossistemas
+		vulneráveis, incerteza e evolução tecnológica e dinâmicas territoriais e sociais
+	</h3>
+	<h4 class="text-xl font-bold pt-2">
+		Coordenação: Teresa Fidélis<br />Relatores: Jorge Cancela
+	</h4>
+	<h4 class="text-xl font-bold pt-2">Síntese das questões abordadas:</h4>
+	<p>
+		Na mesa temática anterior emergiram quatro critérios relevantes para a triagem das opções
+		estratégicas, designadamente a prevenção de riscos naturais, as pegadas sonora e de poluição
+		atmosférica, a salvaguarda das áreas naturais e da avifauna, e importância do ordenamento do
+		território e proteção do solo. Tendo presentes estes critérios, nesta mesa temática
+		procuraram-se aprofundar aspetos mais complexos para a análise de opções estratégicas
+		alternativas tais como os custos escondidos e valorização dos ecossistemas vulneráveis, a
+		incerteza e evolução tecnológica e as dinâmicas territoriais e sociais.
+	</p>
+	<p class="font-bold">Sobre os custos escondidos e valorização dos sistemas vulneráveis</p>
+	<p>Como fundamentar a sua importância na triagem das opções estratégicas?</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>
+			Foi considerado fundamental estimar os custos de investimento para substituir os serviços dos
+			ecossistemas afetados. São importantes ao nível dos custos ambientais e das escalas
+			pertinentes (local, regional, nacional) aspetos como a afetação dos serviços dos ecossistemas,
+			a degradação ambiental, a saúde humana (ruído e emissões), a perda do solo (com potencial
+			económico) e qualidade de vida (mobilidade).
+		</li>
+		<li>
+			Referiu-se também que alguns custos escondidos têm, ou podem ter, impacto decisivo no sucesso
+			e adequação da infraestrutura aeroportuária, podendo ter natureza variada, incluindo custos
+			não mensuráveis, custos mensuráveis (mas ainda não identificados), custos não internalizados.
+		</li>
+		<li>
+			Foram referidos como exemplos de custos escondidos as externalidades ambientais difíceis de
+			quantificar, a saúde pública devido aos fatores de ruído e poluição ambiental, oportunidades
+			de melhoramento (custo do não melhoramento), congestionamento ou produtividade, custos
+			ambientais decorrentes das deslocações dos trabalhadores, soluções técnicas de implementação,
+			exploração e manutenção, custos de adaptação climática (migração climática, adaptação à subida
+			do Nível Médio do Mar, maior escassez nas reservas de água devido ao aumento dos períodos de
+			seca, ondas de calor que implicam maior esforço de climatização e adaptação dos edifícios),
+			rede logística e de distribuição de energia, escassez ou esgotamento de recursos minerais
+			(terras raras) que inviabilizem soluções tecnológicas adotadas a custos viáveis ou, por
+			último, custos de manutenção das infraestruturas aeroportuárias e de atividades conexas, para
+			adaptação às alterações climáticas e subida do nível do mar.
+		</li>
+	</ol>
+	<p>Como analisar e com que indicadores?</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>
+			Importa definir as condições de fronteira entre custos visíveis e escondidos. Pode haver
+			variação significativa em função da informação disponível, do tempo de análise e do âmbito
+			territorial.
+		</li>
+		<li>
+			Face à evolução da exploração aeroportuária e à dinâmica dos ecossistemas, particular ênfase
+			deve ser dada aos mecanismos de monitorização e avaliação, por forma a serem possíveis medidas
+			compensatórias evolutivas ao longo do ciclo de vida da infraestrutura.
+		</li>
+		<li>
+			A abordagem de análise terá de privilegiar a gestão adaptativa das respostas aos impactos em
+			função dos resultados da monitorização e avaliação.
+		</li>
+		<li>
+			Temas reconhecidamente importantes no domínio da ecologia e dos impactes sobre os ecossistemas
+			incluem a poluição luminosa, o ruído, a fragmentação dos habitats, a homogeneização de usos do
+			solo, a introdução de pragas, doenças e espécies invasoras.
+		</li>
+		<li>
+			Necessidade de definir limiares que, resultando de dispositivos legais, excluem determinadas
+			opções de localização.
+		</li>
+		<li>
+			Analisando a incerteza e variabilidade e ponderando impactos, utilizando matrizes de análise
+			multicritério, e recorrendo aos indicadores específicos (objetivos e subjetivos).
+		</li>
+	</ol>
+	<p>Que cenários futuros devem ser considerados?</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>
+			Aqueles em que os custos escondidos se revelem com grau plausível de criticidade. Em
+			particular as projeções demográficas, as emissões poluentes e as alterações climáticas.
+		</li>
+		<li>
+			A evolução tecnológica tem impacto sobre a expressão de diferentes tipologias de custos
+			escondidos.
+		</li>
+		<li>
+			Deve ser considerada a elevada incerteza dos cenários climáticos nos impactos sobre as
+			populações e sobre os ecossistemas, tanto na ótica da mitigação como de adaptação.
+		</li>
+		<li>
+			Efeito “cascata” de fatores climáticos, ambientais e riscos naturais (sismos, tsunamis,
+			temporais, etc.), devido a possíveis correlações não conhecidas ou identificadas.
+		</li>
+		<li>
+			Como resultado do cruzamento das diferentes matrizes de risco, seria muito útil a elaboração
+			de uma carta de aptidão aeroportuária, multicamada, que integrasse toda a informação através
+			de um “heat map”.
+		</li>
+	</ol>
+	<p class="font-bold">Sobre a incerteza e evolução tecnológica</p>
+	<p>Como fundamentar a sua importância na triagem das opções estratégicas?</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>
+			As incertezas ambientais e climáticas, e as questões tecnológicas, principalmente devido à sua
+			crescente alterabilidade e evolução acelerada, podem modificar condições mais ou menos
+			estruturais e operacionais, senão mesmo paradigmas fundacionais, donde impactar diretamente a
+			escolha. Entre estas incertezas encontram-se a procura do transporte aéreo, a evolução da rede
+			de transportes, a evolução de fontes energéticas, o desempenho das aeronaves e demais
+			tecnologia aeronáutica, a evolução do controlo do espaço aéreo, a evolução climática em termos
+			globais e regionais ou o impacto da localização no ecossistema local.
+		</li>
+	</ol>
+	<p>Como analisar e com que indicadores?</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>Criação de matrizes de risco (com ponderação objetiva e subjetiva).</li>
+		<li>
+			Incluir as incertezas na amplitude dos fatores de impacto (range das entradas nas matrizes de
+			risco).
+		</li>
+		<li>
+			Devem incluir indicadores alargados para monitorizar evolução futura: alterações nos padrões
+			de comportamento de compra, semana dos 4 dias, viagens de avião mais longas e por tempos mais
+			longos, AI e robótica nos sistemas.
+		</li>
+		<li>
+			Questões que sevem ser equacionadas - que profissões existirão no futuro? que mobilidade será
+			necessária? que alternativas energéticas estarão operacionais? que cenários futuros devem ser
+			considerados?
+		</li>
+		<li>Riscos de pandemias transversais que afetem ecossistemas sensíveis.</li>
+		<li>
+			Tomar os cenários decorrentes da análise das matrizes citadas, mancha de maior probabilidade e
+			sua correlação com outros fatores (importância estratégica, questões económicas, fatores de
+			afetividade coletiva). Em complementaridade, considerar também os cenários laterais extremos,
+			mais otimista e mais pessimista (com estrita relação com os “custos escondidos”).
+		</li>
+	</ol>
+	<p class="font-bold">Sobre as dinâmicas territoriais e sociais</p>
+	<p>Como fundamentar a sua importância na triagem das opções estratégicas?</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>
+			É determinante para se perceber o resultado de benefícios e prejuízos para a população
+			circunvizinha. Trata-se de uma dimensão fundamental para determinar estruturas económicas,
+			eficiências do uso do solo, sistema urbano e coesão socio-territorial. É fundamental para se
+			perceber e projetar pressões, fluxos, distribuição, cargas de outra natureza, que contribuem
+			para fomentar (ou não) o equilíbrio territorial.
+		</li>
+		<li>
+			Importante para se encontrar uma solução em que a oferta aeroportuária não fragmente o
+			território, se articule com as redes de transporte existentes e a construir de forma a
+			integrar e otimizar as redes de transporte (minimizando o impacto das deslocações) e de
+			energia, preserve o território e espaços classificados (ex.: reservas naturais e agrícola
+			nacional), e contribua para a geração e distribuição de riqueza.
+		</li>
+		<li>Relevante na prevenção de pressão sobre habitats e ecossistemas.</li>
+		<li>Relevante no esforço de adaptação climática.</li>
+		<li>
+			Atender à capacidade indutora do processo de revisão dos IGT e correspondentes modelos de
+			desenvolvimento.
+		</li>
+	</ol>
+	<p>Como analisar e com que indicadores?</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>
+			Necessidade de definir limiares que, resultando de dispositivos legais, excluem determinadas
+			opções de localização de novas infraestruturas aeroportuárias tais como, universo de pessoas
+			afetadas (positiva e negativamente), dinâmicas económicas, pressões pendulares e migratórias,
+			custos e tempos de deslocações, custos relativos ao fornecimento de energia versus eficiência
+			energética futura, saúde pública (decorrente do impacto ambiental), universo de habitats e
+			ecossistemas afetados, impacto de adaptação climática e esforço de compensação ambiental, solo
+			urbano disponível.
+		</li>
+	</ol>
+	<p>Que cenários futuros devem ser considerados?</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>
+			O desenho de cenários futuros deverá ter em conta diretrizes, planos, compromissos
+			estabelecidos e expetativas de procura.
+		</li>
+		<li>
+			Deverá também ter em conta avaliação dos cenários de dinâmica demográfica (sobretudo migração
+			do interior para o litoral), a realidade imobiliária e a respetiva relevância para o
+			desenvolvimento do território, económico (nomeadamente quanto ao turismo).
+		</li>
+		<li>Cenários de maior ou menor pressão dessas dinâmicas sobre habitas e ecossistemas.</li>
+		<li>
+			Cenários de deslocados climáticos, devido a medidas de adaptação (ex.: necessidade de
+			relocalização de populações costeiras para o interior).
+		</li>
+	</ol>
 </PageContainer>
--- src/routes/(estrategia-de-participacao)/mesas-tematicas/4/+page.svelte
+++ src/routes/(estrategia-de-participacao)/mesas-tematicas/4/+page.svelte
@@ -34,8 +34,12 @@
 		Esta mesa temática irá discutir os possíveis modelos de aeroporto e o seu papel no
 		desenvolvimento económico, numa perspectiva estratégica.
 	</p>
-	<h3 class="text-2xl font-bold pt-6">Resultados 1ª Sessão</h3>
-	<h4 class="text-xl font-bold pt-2">Coordenação: Rosário Macário<br />Relator: Vitor Rocha</h4>
+	<h3 class="text-2xl font-bold pt-6">
+		Resultados 1ª Sessão<br />Aeroporto e desenvolvimento terrritorial
+	</h3>
+	<h4 class="text-xl font-bold pt-2">
+		Coordenação: Rosário Macário<br />Relatores: Eng. Vasco Reis e Eng. Vitor Rocha
+	</h4>
 	<h4 class="text-xl font-bold pt-2">Ideias chave:</h4>
 	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
 		<li>
@@ -116,5 +120,242 @@
 		Por fim, foi destacada a importância de integrar o transporte público de passageiros na rede de
 		transportes terrestres, assegurando a integração com a ferrovia e compatibilizando com o plano
 		nacional ferroviário.
+	</p>
+	<h3 class="text-2xl font-bold pt-6">
+		Resultados 2ª Sessão<br />Modelo Dual versus Modelo Single, quais as opções de transição?
+	</h3>
+	<h4 class="text-xl font-bold pt-2">
+		Coordenação: Rosário Macário<br />Relatores: Eng. Vasco Reis e Eng. Vitor Rocha
+	</h4>
+	<p>
+		As várias opções foram discutidas em três mesas rotativas, permitindo a todos os participantes
+		discutir e identificar as vantagens e inconvenientes das opções consideradas, que foram: um
+		aeroporto Greenfield; uma opção Dual; Solução de transição.
+	</p>
+	<h4 class="text-xl font-bold pt-2">Ideias chave:</h4>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>
+			A construção do novo aeroporto deve seguir uma lógica flexível (isto é, ter as componentes
+			mínimas necessárias para entrar em funcionamento);
+		</li>
+		<li>
+			Foi consensual que, no longo prazo, a região de Lisboa deverá ser servida por um único
+			aeroporto hub. A existência de um único aeroporto é necessário para permitir a concentração de
+			serviços e atividades numa única região e, desta forma, maximizar os benefícios do hubbing;
+		</li>
+		<li>
+			A opção dual temporária é necessária para evitar a interrupção do fluxo aéreo em Lisboa
+			durante a construção do novo aeroporto;
+		</li>
+		<li>
+			A melhoria na ligação com outros modos de transporte também pode alargar a catchment area do
+			green field. Adicionalmente, há claras vantagens ambientais, de saúde pública e de risco de
+			acidente;
+		</li>
+		<li>
+			A única opção para a manutenção da Portela seria como vertiport ou com um horário de
+			funcionamento extremamente reduzido e limitado (à semelhança do aeroporto de London City).
+		</li>
+	</ol>
+	<h4 class="text-xl font-bold pt-2">Sumário da discussão dos participantes</h4>
+	<p class="font-bold">Mesa 1: Opção Green Field</p>
+	<p>
+		Esta mesa debateu se o novo aeroporto de Lisboa deve ser construído como um green field airport.
+		Entre as vantagens de um green field airport estão a visão de longo prazo, que não se limita à
+		salvaguarda do turismo, e a oportunidade de promover o desenvolvimento econômico, captando
+		saberes e novas tecnologias. Além disso, esse tipo de aeroporto pode ser uma alavanca para
+		alterar o perfil económico da região, permitindo a junção das LCC e das companhias de bandeira
+		para consolidar o HUB. Isso é importante, pois só há possibilidade de sobrevivência do HUB no
+		longo prazo com uma solução que junte o médio curso e o longo curso, permitindo alavancar novas
+		rotas no longo curso. A melhoria na ligação com outros modos de transporte também pode alargar a
+		catchment area do green field. Adicionalmente, há claras vantagens ambientais, de saúde pública
+		e de risco de acidente.
+	</p>
+	<p>
+		Por outro lado, as desvantagens incluem o investimento e tempo de execução de obra em comparação
+		com uma proposta dual, o que só faz sentido se analisado em termos de curto prazo. A mudança do
+		HUB em primeiro lugar obriga a fechar a Portela para a mudança da LCC, o que pode ser um desafio
+		para uma transição bem ponderada. Além disso, o tempo de transição para uma solução green field
+		pode ser mais longo devido às dificuldades em investimento e tempo de construção. Existe um
+		risco de investimento demasiado grande, e torna-se necessário encontrar soluções para
+		suportá-lo. Pode existir uma afetação do passageiro sensível ao tempo e ao custo, o que pode
+		condicionar a decisão do governo. Maior dificuldade no faseamento da transição para um novo
+		aeroporto também é uma desvantagem importante.
+	</p>
+	<p>
+		Dessa forma, é importante que o governo e/ou o potencial investidor façam algum esforço para
+		minimizar o crescimento de taxas e atender às necessidades do turismo durante a transição.
+		Embora seja necessário ponderar todos os prós e contras, é crucial encontrar soluções para
+		suportar o investimento e garantir o sucesso a longo prazo do novo aeroporto de Lisboa.
+	</p>
+	<p class="font-bold">Mesa 2: Opção Dual</p>
+	<p>Esta mesa debateu a opção dual. Várias opções duais foram analisadas, nomeadamente:</p>
+	<ol class="marker:text-blue-accent marker:font-bold list-decimal list-outside pl-10">
+		<li>
+			<p><b>Hub:</b> Novo Aeroporto</p>
+			<p><b>Secundário:</b> Portela</p>
+		</li>
+		<li>
+			<p><b>Hub:</b> Portela</p>
+			<p><b>Secundário:</b> Novo Aeroporto</p>
+		</li>
+		<li>
+			<p><b>Hub:</b> Novo Aeroporto</p>
+			<p><b>Secundário:</b> Portela (com phase out após construção do Novo Aeroporto)</p>
+		</li>
+		<li>
+			<p><b>Hub:</b> Novo Aeroporto</p>
+			<p><b>Secundário:</b> Portela mais 1 temporário enquanto Novo Aeroporto é construído</p>
+		</li>
+	</ol>
+	<p>
+		A opção #3 foi consensualmente escolhida – ou seja, a existência de uma solução dual temporária
+		até à entrada em funcionamento do Novo Aeroporto. Neste cenário o Novo Aeroporto deverá ser
+		posto em funcionamento o mais rapidamente possível e com os componentes mínimos necessários. Ou
+		seja, a construção do novo aeroporto deve seguir uma lógica flexível (isto é, ter as componentes
+		mínimas necessárias para entrar em funcionamento).
+	</p>
+	<p>
+		A opção dual temporária é necessária para evitar a interrupção do fluxo aéreo em Lisboa durante
+		a construção do novo aeroporto. A adaptação ou construção de um aeroporto secundário para
+		permitir a transição, até à construção do novo aeroporto (Opção #4) não foi considerada viável
+		por vários motivos, nomeadamente: custo afundado elevado, com hipóteses mínimas de recuperação
+		no tempo de transição; ganhos de eficiência e eficácia operacional questionáveis; e o risco de
+		as soluções temporárias se cristalizarem e, desta forma, protelarem a construção do Novo
+		Aeroporto.
+	</p>
+	<p>
+		De facto, foi algo consensual que, no longo prazo, a região de Lisboa deverá ser servida por um
+		único aeroporto hub. A existência de um único aeroporto é necessário para permitir a
+		concentração de serviços e atividades numa única região e, desta forma, maximizar os benefícios
+		do hubbing. A opção dual limitaria o efeito de concentração e reduziria a natureza e a
+		quantidade de serviços adicionais em cada um dos aeroportos. A solução dual pode impor várias
+		limitações à criação de novos serviços, já que a Portela não tem capacidade para expandir. A
+		solução dual implica uma maior carga ambiental do que a solução de um único aeroporto.
+	</p>
+	<p>
+		Foi ainda discutido de que não há exemplos comparáveis de regiões como Lisboa (em termos de
+		população) que possuam dois aeroportos internacionais. Aeroportos menores ou regionais são
+		suficientes para atender às necessidades da região.
+	</p>
+	<p>
+		Ainda assim foram reconhecidas algumas vantagens numa opção dual. A existência de dois
+		aeroportos pode fornecer mais opções e flexibilidade para lidar com a variedade de segmentos
+		aéreos. A manutenção da Portela permite a continuidade dos serviços prestados pelas companhias
+		aéreas de baixo custo, que preferem a localização do aeroporto da Portela. A existência de dois
+		aeroportos pode fornecer opções adicionais para serviços de logística e outras empresas de
+		serviços de valor agregado. Na eventualidade de haver uma opção dual, a maioria dos
+		participantes concordaram que a Portela deveria ser desativada. O aeroporto secundário poderá
+		facilmente ser oferecido por um dos aeroportos existentes na região de Lisboa.
+	</p>
+	<p>
+		A única opção para a manutenção da Portela seria como vertiport ou com um horário de
+		funcionamento extremamente reduzido e limitado (à semelhança do aeroporto de London City).
+	</p>
+	<p class="font-bold">Mesa 3: Transição</p>
+	<p>
+		Nesta mesa foi debatida o modelo de transição do atual aeroporto da Portela para a solução
+		futura, independentemente de ser greenfield ou dual.
+	</p>
+	<p>
+		No caso da opção greenfield foram debatidas duas abordagens discutidas pelos participantes.
+		Alguns participantes defenderam uma transição imediata para o novo aeroporto, argumentando que,
+		apesar do receio, existem exemplos passados de sucesso em outros casos de aeroportos (como em
+		Munique e Denver) e que é necessário avançar. A questão dos custos da transição seria dividida
+		entre o governo ou os passageiros. Nessa abordagem, as decisões sobre uma única ou dupla
+		configuração seriam mais importantes. Outros participantes defenderam uma transição em fases,
+		com a maioria concordando que o sistema duplo deveria ser adotado até que o novo aeroporto
+		estivesse pronto, com um horizonte de tempo de seis anos. Foi enfatizado que uma transição em
+		fases deveria ter um cronograma claro e firme, que os envolvidos pudessem confiar e usar como
+		base para suas operações. A colaboração e a confiança entre as partes interessadas foram
+		apontadas como fundamentais nesse caso.
+	</p>
+	<p>
+		Para a opção dual, alguns participantes apoiam a transferência das companhias aéreas de baixo
+		custo (LCCs) para o novo aeroporto enquanto mantêm o hub na infraestrutura existente, enquanto
+		outras apoiam a transferência do hub para o segundo aeroporto. No caso de manter o hub em
+		Portela, o segundo aeroporto poderia ser usado como meio de desenvolvimento, explorando novas
+		rotas e cenários de crescimento. No entanto, a existência de operações da Easyjet no Terminal 1
+		complica a transferência completa de suas operações para o segundo aeroporto. Foi destacado que
+		os viajantes de negócios desejam estar em Lisboa e ter acesso rápido à cidade com o mínimo de
+		incerteza. Foi enfatizada a importância de não dividir as operações da companhia aérea em dois
+		aeroportos, concentrando as operações de uma companhia aérea em um aeroporto. Um dos desafios
+		para Portela durante a transição será o desempenho ambiental e a capacidade de ar. Para o
+		segundo aeroporto, o desenvolvimento imobiliário pode ser um desafio. Os requisitos de energia e
+		os aspetos ambientais, como o ruído, devem ser considerados, assim como as medidas para
+		mitigá-los. Uma sugestão é usar o segundo aeroporto para impor restrições nos níveis de ruído no
+		primeiro durante a noite (e possivelmente durante o dia). Para o operador, o desafio geral será
+		não conseguir gerir com dois grandes aeroportos e duplicar a escala dos problemas atuais. Além
+		disso, ambos os aeroportos devem ser eficientes em termos de operações e conectividade. Os
+		custos devem considerar o risco de serem duplicados nesse caso (variável+funcionários+manuseio
+		etc).
+	</p>
+	<p>
+		Em ambos os casos, para facilitar o processo de transição, foi sugerido um relaxamento das taxas
+		aeroportuárias, mas também a necessidade de regulamentação das taxas, embora os participantes
+		não confiassem na possibilidade de uma mudança radical na regulamentação devido a aspetos
+		culturais e experiências anteriores. Nenhuma companhia aérea vai querer ser a primeira a testar
+		a transferência das operações para o novo terminal, as companhias aéreas devem ser encorajadas a
+		mudarem-se para outro aeroporto.
+	</p>
+	<p>
+		Deverá ser realizada uma análise de risco das operações e uma análise custo-benefício
+		considerando os benefícios económicos e sociais para o país. Os riscos relacionados com questões
+		legais poderão surgir devido à preservação da vida selvagem. A evolução do turismo e a exigência
+		dos intervenientes do turismo poderiam também fazer surgir riscos. Finalmente, atrair
+		investidores seria um desafio em ambos os casos, mas por diferentes razões.
+	</p>
+	<h3 class="text-2xl font-bold pt-6">
+		Resultados 3ª Sessão<br />Conectividade na rede aérea e dependência do hinterland
+	</h3>
+	<h4 class="text-xl font-bold pt-2">
+		Coordenação: Rosário Macário<br />Relatores: Vasco Reis e Sofia Kalakou
+	</h4>
+	<h4 class="text-xl font-bold pt-2">Ideias chave:</h4>
+	<ol class="marker:text-blue-accent marker:font-bold list-disc list-outside pl-10">
+		<li>O hub permite ligações diretas (conectividade) a um maior número de destinos.</li>
+		<li>
+			O atual contexto de privatização da TAP terá impacte sobre o desenvolvimento do hub em Lisboa.
+		</li>
+	</ol>
+	<h4 class="text-xl font-bold pt-2">Sumário da discussão dos participantes</h4>
+	<p class="font-bold">O hub é bom ou mau para o caso português?</p>
+	<p>O hub é a melhor opção e positiva para o caso português.</p>
+	<p>
+		O hub permite ligações diretas (conetividade) a maior número de destinos. O hub da TAP em Lisboa
+		tem sido um dos motores do desenvolvimento turístico de Lisboa. A TAP oferece a possibilidade
+		stopover, em Lisboa, de alguns dias, o que tem permitido a muitos passageiros ficarem a conhecer
+		Lisboa, e voltarem. Assim, o hub é bom para o caso português. Aliás, o atual aeroporto, mesmo
+		sem condições, é já um hub reconhecido.
+	</p>
+	<p>
+		O hub é desenvolvido por uma companha aérea, e não por pelo gestor de infraestrutura. O
+		desenvolvimento do hub depende de uma companhia que estabeleça a base no novo aeroporto de
+		Lisboa. Sem companhia de transporte aéreo não há hub. Portanto, o atual contexto de privatização
+		da TAP lança dúvidas sobre a existência de um hub em Lisboa. Em função do investidor poderá não
+		fazer sentido a existência de hub (ex.: se o comprador for a IAG, os participantes referem que
+		não haverá hub em Lisboa).
+	</p>
+	<p>
+		Na ausência de uma companhia aérea que faça de Lisboa a base, o aeroporto poderá não ser hub. A
+		sua capacidade ficará ocupada por companhias que o utilizam para voos ponto a ponto. Neste caso,
+		o tipo de voos e passageiros serão naturalmente diferentes. Os voos serão principalmente de
+		média distância, ou low cost ou <i>feeder</i> para outro hub. E haverá redução de conetividade. Outros
+		fatores que influenciam a existência de hub, é o crescimento do tráfego aéreo, que nos próximos anos
+		poderá ser limitado por vários fatores, includingo restrições ambientais, custos do combustível ou
+		outros.
+	</p>
+	<p>
+		Uma vantagem de Lisboa no território europeu é que pode servir a Europa, África e América. Isto
+		pode ser especialmente importante quando se consideram os impactos ambientais da aviação e o
+		facto de os voos que partem de Lisboa exigirem uma hora a menos em comparação com outros
+		aeroportos europeus. Os participantes salientaram que os pontos de corte para os níveis de
+		emissões em viagens de curta, média e longa distância devem ser explorados juntamente com a
+		possibilidade de utilizar aeronaves de corpo estreito para voos directos. Relativamente ao
+		interior, deve ser considerada uma área de captação de 2-3h (de carro), mas, em comparação com
+		outros aeroportos da UE, para Portugal, o interior terá sempre limitações. Finalmente, se um hub
+		pudesse assegurar uma capacidade aérea suficiente, as companhias aéreas poderiam explorar novas
+		oportunidades de negócio com a utilização de aeronaves para servir múltiplos pontos antes de
+		regressarem ao seu aeroporto "de origem".
 	</p>
 </PageContainer>
