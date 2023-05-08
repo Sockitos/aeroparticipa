ffe9582c7a0a702ac079748b18ea3b7e2c435b84
João Nogueira <jrnogueira@edu.ulisboa.pt>
2023-05-08 16:30:09 +0100

    Add fathom analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import Footer from '@/components/Footer.svelte';
 	import Header from '@/components/Header.svelte';
 	import Popup from '@/components/Popup.svelte';
+	import * as Fathom from 'fathom-client';
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	onMount(() => {
+		Fathom.load('CGQSLBEF', { includedDomains: ['aeroparticipa.pt', 'www.aeroparticipa.pt'] });
+	});
+
+	$: $page.url.pathname, browser && Fathom.trackPageview();
 </script>
 
 <div class="min-h-screen flex flex-col">
