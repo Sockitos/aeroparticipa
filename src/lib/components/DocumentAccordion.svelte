a3df2588c204bb4fdb4c399f926e0b6b516f4da8
João Nogueira <jrnogueira@edu.ulisboa.pt>
2023-11-28 14:52:59 +0000

    Add example with document accordion

---
+++ src/lib/components/DocumentAccordion.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+
+	let expanded = false;
+</script>
+
+<button
+	on:click={() => (expanded = !expanded)}
+	class="text-blue-accent text-xl font-bold hover:text-blue-light flex flex-row items-center justify-center gap-x-2"
+>
+	Ver Documentos
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 96 960 960"
+		class="fill-blue-accent h-5 transition-transform"
+		class:rotate-90={expanded}
+	>
+		<path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+	</svg>
+</button>
+
+{#if expanded}
+	<div transition:slide>
+		<slot />
+	</div>
+{/if}
--- src/routes/relatorios/+page.svelte
+++ src/routes/relatorios/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { base } from '$app/paths';
+	import DocumentAccordion from '@/components/DocumentAccordion.svelte';
 	import DocumentLink from '@/components/DocumentLink.svelte';
 	import PageContainer from '@/components/PageContainer.svelte';
 	import PageHeader from '@/components/PageHeader.svelte';
@@ -28,13 +29,33 @@
 	<div class="flex flex-col gap-4 mt-5">
 		<DocumentLink href="{base}/relatorios/relatorio_2.pdf">Documento</DocumentLink>
 		<DocumentLink href="{base}/relatorios/relatorio_2.pdf">Sumário Executivo</DocumentLink>
-		<DocumentLink href="{base}/relatorios/relatorio_2_1.pdf">Documento pós Consulta Pública</DocumentLink>
+		<DocumentLink href="{base}/relatorios/relatorio_2_1.pdf"
+			>Documento pós Consulta Pública</DocumentLink
+		>
 	</div>
 	<h3>Relatório da Análise de Curto Prazo</h3>
-    <p>
-		A comissão técnica independente disponibiliza o Relatório da Análise de Curto Prazo da situação atual do 
-		Aeroporto Humberto Delgado (AHD), onde consta uma proposta de ações de melhoria da sua eficiência e eficácia operacional.
-    </p>
-    <div class="flex flex-col gap-4 mt-5">
-        <DocumentLink href="{base}/relatorios/relatorio_AHD.pdf">Documento</DocumentLink>
+	<p>
+		A comissão técnica independente disponibiliza o Relatório da Análise de Curto Prazo da situação
+		atual do Aeroporto Humberto Delgado (AHD), onde consta uma proposta de ações de melhoria da sua
+		eficiência e eficácia operacional.
+	</p>
+	<div class="flex flex-col gap-4 mt-5">
+		<DocumentLink href="{base}/relatorios/relatorio_AHD.pdf">Documento</DocumentLink>
+	</div>
+	<h3>Relatório Exemplo</h3>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+		labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+		laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+		voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+		non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	</p>
+	<DocumentAccordion>
+		<div class="flex flex-col gap-4 mt-5">
+			<DocumentLink href="{base}/relatorios/relatorio_AHD.pdf">Documento</DocumentLink>
+			<DocumentLink href="{base}/relatorios/relatorio_AHD.pdf">Documento</DocumentLink>
+			<DocumentLink href="{base}/relatorios/relatorio_AHD.pdf">Documento</DocumentLink>
+			<DocumentLink href="{base}/relatorios/relatorio_AHD.pdf">Documento</DocumentLink>
+		</div>
+	</DocumentAccordion>
 </PageContainer>
