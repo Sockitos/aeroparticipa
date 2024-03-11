b392d0583875cf2a98af636f4abc58fb4959d9c1
Dysva <danielandresilva@e-tecnico.ulisboa.pt>
2024-03-11 13:50:59 +0000

    Create +page.svelte

---
+++ src/routes/relatorio-ambiental/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { base } from '$app/paths';
+	import DocumentLink from '@/components/DocumentLink.svelte';
+	import PageContainer from '@/components/PageContainer.svelte';
+	import PageHeader from '@/components/PageHeader.svelte';
+</script>
+
+<svelte:head>
+	<title>AeroParticipa - Relatórios</title>
+</svelte:head>
+
+<PageHeader>Relatórios</PageHeader>
+<PageContainer>
+	<h3>Relatório Preliminar da CTI</h3>
+	<p>
+		mega teste
+	</p>
+	
+	
+	
+</PageContainer>
